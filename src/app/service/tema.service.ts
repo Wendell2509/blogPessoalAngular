29681de3ccecab36e5944996f03d01926ab2b6f7
Wendell <wendellfranco99@gmail.com>
2021-11-10 15:03:38 -0300

    Implemented get and post Temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  //RECEBE TOKEN
+  token = {
+    headers: new HttpHeaders().set('Authorization',environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
+  //RECEBE TODOS OS TEMAS
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://blogpessoalwendellfranco.herokuapp.com/temas/all',this.token)
+  }
+
+  //CRIA UM NOVO TEMA
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://blogpessoalwendellfranco.herokuapp.com/temas/save',tema,this.token)
+  }
+
+}

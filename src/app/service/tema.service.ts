7e2fe1e2be6e63e8012956c8cb6b77a09e9a29b4
Wendell <wendellfranco99@gmail.com>
2021-11-10 19:55:30 -0300

    Implementing put and delete tema and creating html and ts pages

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -16,6 +16,7 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization',environment.token)
   }
 
+  //ATUALIZA O TOKEN
   refreshToken(){
     this.token = {
       headers: new HttpHeaders().set('Authorization', environment.token)
@@ -27,9 +28,23 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoalwendellfranco.herokuapp.com/temas/all',this.token)
   }
 
+  getByIdTema(id: number):Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoalwendellfranco.herokuapp.com/temas/${id}`,this.token)
+  }
+
   //CRIA UM NOVO TEMA
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalwendellfranco.herokuapp.com/temas/save',tema,this.token)
+  }
+
+  //ATUALIZA O NOME DE UM TEMA
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoalwendellfranco.herokuapp.com/temas/update',tema,this.token)
+  }
+
+  //DELETA UM TEMA E TODOS AS POSTAGENS DEPENDENTES DESSE TEMA
+  deleteTema(id: number){
+    return this.http.delete(`https://blogpessoalwendellfranco.herokuapp.com/temas/delete/${id}`,this.token)
   }
 
 }
